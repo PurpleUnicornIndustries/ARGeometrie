4089cdfb08bcf1a685ef87bb43c71c75dbfd9426
PurpleUnicornIndustries <66017177+PurpleUnicornIndustries@users.noreply.github.com>
2020-05-27 15:43:07 +0200

    Add files via upload

---
+++ RaumlicheBeziehung/__settings__.js
@@ -0,0 +1,20 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "910623.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 30238675, 30238664, 30238885, 30238653, 30238649, 30238672, 30238648, 30238652, 30263901, 31337500, 31337501 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
