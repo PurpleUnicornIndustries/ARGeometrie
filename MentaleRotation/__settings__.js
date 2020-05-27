5f3a0b4c2fbf36e0183cb0bce19d0e311543ffda
PurpleUnicornIndustries <66017177+PurpleUnicornIndustries@users.noreply.github.com>
2020-05-27 15:34:13 +0200

    Add files via upload

---
+++ MentaleRotation/__settings__.js
@@ -0,0 +1,20 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "909922.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 29353735, 29353741, 30193027, 30193531, 30193532, 30193534, 30193535, 30193667, 30265465, 31317267, 31326397 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
