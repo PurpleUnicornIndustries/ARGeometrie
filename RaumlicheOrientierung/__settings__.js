f00fe07d6c10a944cbed394a941af87ad3fcb634
Ivvey <39462035+Ivvey@users.noreply.github.com>
2020-05-29 16:07:23 +0200

    1

---
+++ RaumlicheOrientierung/__settings__.js
@@ -0,0 +1,20 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "928720.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 31344681, 31344676, 31344678, 31344683, 31344669, 31344657, 31344695, 31344725, 31344731, 31344723, 31344721 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
